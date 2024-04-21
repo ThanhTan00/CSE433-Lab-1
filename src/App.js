a6faba6cc72db14edc38c4b7e5e9d00d4d22c107
ThanhTan00 <tan.lenguyen.cit19@eiu.edu.vn>
2024-04-21 16:23:09 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
+=======
+import logo from "./logo.svg";
+import "./App.css";
+import "./Product";
+import { BannerCard } from "./BannerCard";
+import { ProductCard } from "./Product";
+>>>>>>> Stashed changes
 
 function App() {
   return (
@@ -36,7 +44,7 @@ function App() {
             <nav className="limiter-menu-desktop container">
               
               <a href="#" className="logo">
-                <img src="images/icons/logo-01.png" alt="IMG-LOGO"></img>
+                <img src="images/icons/logo-01.png" alt="IMG-LOGO" />
               </a>
 
               <div className="menu-desktop">
@@ -91,7 +99,13 @@ function App() {
 
         <div className="wrap-header-mobile">
           <div className="logo-mobile">
+<<<<<<< Updated upstream
             <a href="index.html"><img src="images/icons/logo-01.png" alt="IMG-LOGO"></img></a>
+=======
+            <a href="index.html">
+              <img src="images/icons/logo-01.png" alt="IMG-LOGO" />
+            </a>
+>>>>>>> Stashed changes
           </div>
 
           <div className="wrap-icon-header flex-w flex-r-m m-r-15">
@@ -182,14 +196,23 @@ function App() {
         <div className="modal-search-header flex-c-m trans-04 js-hide-modal-search">
           <div className="container-search-header">
             <button className="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
-              <img src="images/icons/icon-close2.png" alt="CLOSE"></img>
+              <img src="images/icons/icon-close2.png" alt="CLOSE" />
             </button>
 
             <form className="wrap-search-header flex-w p-l-15">
               <button className="flex-c-m trans-04">
                 <i className="zmdi zmdi-search"></i>
               </button>
+<<<<<<< Updated upstream
               <input className="plh3" type="text" name="search" placeholder="Search..."></input>
+=======
+              <input
+                className="plh3"
+                type="text"
+                name="search"
+                placeholder="Search..."
+              />
+>>>>>>> Stashed changes
             </form>
           </div>
         </div>
@@ -213,7 +236,7 @@ function App() {
             <ul className="header-cart-wrapitem w-full">
               <li className="header-cart-item flex-w flex-t m-b-12">
                 <div className="header-cart-item-img">
-                  <img src="images/item-cart-01.jpg" alt="IMG"></img>
+                  <img src="images/item-cart-01.jpg" alt="IMG" />
                 </div>
 
                 <div className="header-cart-item-txt p-t-8">
@@ -229,7 +252,7 @@ function App() {
 
               <li className="header-cart-item flex-w flex-t m-b-12">
                 <div className="header-cart-item-img">
-                  <img src="images/item-cart-02.jpg" alt="IMG"></img>
+                  <img src="images/item-cart-02.jpg" alt="IMG" />
                 </div>
 
                 <div className="header-cart-item-txt p-t-8">
@@ -245,7 +268,7 @@ function App() {
 
               <li className="header-cart-item flex-w flex-t m-b-12">
                 <div className="header-cart-item-img">
-                  <img src="images/item-cart-03.jpg" alt="IMG"></img>
+                  <img src="images/item-cart-03.jpg" alt="IMG" />
                 </div>
 
                 <div className="header-cart-item-txt p-t-8">
@@ -282,7 +305,14 @@ function App() {
       <section className="section-slide">
         <div className="wrap-slick1">
           <div className="slick1">
+<<<<<<< Updated upstream
             <div className="item-slick1" style={{backgroundImage: `url(images/slide-01.jpg)`}}>
+=======
+            <div
+              className="item-slick1"
+              style={{ backgroundImage: "url(images/slide-01.jpg)" }}
+            >
+>>>>>>> Stashed changes
               <div className="container h-full">
                 <div className="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                   <div className="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
@@ -305,7 +335,11 @@ function App() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
 
+<<<<<<< Updated upstream
             <div className="item-slick1" style={{backgroundImage: `url(images/slide-02.jpg)`}}>
               <div className="container h-full">
                 <div className="flex-col-l-m h-full p-t-100 p-b-30 respon5">
@@ -324,12 +358,335 @@ function App() {
                   <div className="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
                     <a href="product.html" className="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
                       Shop Now
+=======
+      <div className="sec-banner bg0 p-t-80 p-b-50">
+        <div className="container">
+          <div className="row">
+            <BannerCard
+              title="Women"
+              collection="Spring 2018"
+              image="images/banner-01.jpg"
+            ></BannerCard>
+
+            <BannerCard
+              title="Men"
+              collection="Spring 2018"
+              image="images/banner-02.jpg"
+            ></BannerCard>
+
+            <BannerCard
+              title="Accessories"
+              collection="New Trend"
+              image="images/banner-03.jpg"
+            ></BannerCard>
+          </div>
+        </div>
+      </div>
+      <section className="bg0 p-t-23 p-b-140">
+        <div className="container">
+          <div className="p-b-10">
+            <h3 className="ltext-103 cl5">Product Overview</h3>
+          </div>
+
+          <div className="flex-w flex-sb-m p-b-52">
+            <div className="flex-w flex-l-m filter-tope-group m-tb-10">
+              <button
+                className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1"
+                data-filter="*"
+              >
+                All Products
+              </button>
+
+              <button
+                className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5"
+                data-filter=".women"
+              >
+                Women
+              </button>
+
+              <button
+                className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5"
+                data-filter=".men"
+              >
+                Men
+              </button>
+
+              <button
+                className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5"
+                data-filter=".bag"
+              >
+                Bag
+              </button>
+
+              <button
+                className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5"
+                data-filter=".shoes"
+              >
+                Shoes
+              </button>
+
+              <button
+                className="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5"
+                data-filter=".watches"
+              >
+                Watches
+              </button>
+            </div>
+
+            <div className="flex-w flex-c-m m-tb-10">
+              <div className="flex-c-m stext-106 cl6 size-104 bor4 pointer hov-btn3 trans-04 m-r-8 m-tb-4 js-show-filter">
+                <i className="icon-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-filter-list"></i>
+                <i className="icon-close-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
+                Filter
+              </div>
+
+              <div className="flex-c-m stext-106 cl6 size-105 bor4 pointer hov-btn3 trans-04 m-tb-4 js-show-search">
+                <i className="icon-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-search"></i>
+                <i className="icon-close-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
+                Search
+              </div>
+            </div>
+
+            <div className="dis-none panel-search w-full p-t-10 p-b-15">
+              <div className="bor8 dis-flex p-l-15">
+                <button className="size-113 flex-c-m fs-16 cl2 hov-cl1 trans-04">
+                  <i className="zmdi zmdi-search"></i>
+                </button>
+
+                <input
+                  className="mtext-107 cl2 size-114 plh2 p-r-15"
+                  type="text"
+                  name="search-product"
+                  placeholder="Search"
+                />
+              </div>
+            </div>
+
+            <div className="dis-none panel-filter w-full p-t-10">
+              <div className="wrap-filter flex-w bg6 w-full p-lr-40 p-t-27 p-lr-15-sm">
+                <div className="filter-col1 p-r-15 p-b-27">
+                  <div className="mtext-102 cl2 p-b-15">Sort By</div>
+
+                  <ul>
+                    <li className="p-b-6">
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        Default
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        Popularity
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        Average rating
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <a
+                        href="#"
+                        className="filter-link stext-106 trans-04 filter-link-active"
+                      >
+                        Newness
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        Price: Low to High
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        Price: High to Low
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="filter-col2 p-r-15 p-b-27">
+                  <div className="mtext-102 cl2 p-b-15">Price</div>
+
+                  <ul>
+                    <li className="p-b-6">
+                      <a
+                        href="#"
+                        className="filter-link stext-106 trans-04 filter-link-active"
+                      >
+                        All
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        $0.00 - $50.00
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        $50.00 - $100.00
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        $100.00 - $150.00
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        $150.00 - $200.00
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        $200.00+
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="filter-col3 p-r-15 p-b-27">
+                  <div className="mtext-102 cl2 p-b-15">Color</div>
+
+                  <ul>
+                    <li className="p-b-6">
+                      <span
+                        className="fs-15 lh-12 m-r-6"
+                        style={{ color: "#222" }}
+                      >
+                        <i className="zmdi zmdi-circle"></i>
+                      </span>
+
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        Black
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <span
+                        className="fs-15 lh-12 m-r-6"
+                        style={{ color: "#4272d7" }}
+                      >
+                        <i className="zmdi zmdi-circle"></i>
+                      </span>
+
+                      <a
+                        href="#"
+                        className="filter-link stext-106 trans-04 filter-link-active"
+                      >
+                        Blue
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <span
+                        className="fs-15 lh-12 m-r-6"
+                        style={{ color: "#b3b3b3" }}
+                      >
+                        <i className="zmdi zmdi-circle"></i>
+                      </span>
+
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        Grey
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <span
+                        className="fs-15 lh-12 m-r-6"
+                        style={{ color: "#00ad5f" }}
+                      >
+                        <i className="zmdi zmdi-circle"></i>
+                      </span>
+
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        Green
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <span
+                        className="fs-15 lh-12 m-r-6"
+                        style={{ color: "#fa4251" }}
+                      >
+                        <i className="zmdi zmdi-circle"></i>
+                      </span>
+
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        Red
+                      </a>
+                    </li>
+
+                    <li className="p-b-6">
+                      <span
+                        className="fs-15 lh-12 m-r-6"
+                        style={{ color: "#aaa" }}
+                      >
+                        <i className="zmdi zmdi-circle-o"></i>
+                      </span>
+
+                      <a href="#" className="filter-link stext-106 trans-04">
+                        White
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="filter-col4 p-b-27">
+                  <div className="mtext-102 cl2 p-b-15">Tags</div>
+
+                  <div className="flex-w p-t-4 m-r--5">
+                    <a
+                      href="#"
+                      className="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5"
+                    >
+                      Fashion
+                    </a>
+
+                    <a
+                      href="#"
+                      className="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5"
+                    >
+                      Lifestyle
+                    </a>
+
+                    <a
+                      href="#"
+                      className="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5"
+                    >
+                      Denim
+                    </a>
+
+                    <a
+                      href="#"
+                      className="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5"
+                    >
+                      Streetstyle
+                    </a>
+
+                    <a
+                      href="#"
+                      className="flex-c-m stext-107 cl6 size-301 bor7 p-lr-15 hov-tag1 trans-04 m-r-5 m-b-5"
+                    >
+                      Crafts
+>>>>>>> Stashed changes
                     </a>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
 
+<<<<<<< Updated upstream
             <div className="item-slick1" style={{backgroundImage: `url(images/slide-03.jpg)`}}>
               <div className="container h-full">
                 <div className="flex-col-l-m h-full p-t-100 p-b-30 respon5">
@@ -348,6 +705,55 @@ function App() {
                   <div className="layer-slick1 animated visible-false" data-appear="rotateIn" data-delay="1600">
                     <a href="product.html" className="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
                       Shop Now
+=======
+          <div class="row isotope-grid">
+            <ProductCard
+              name="Esprit Ruffle Shirt"
+              price="16"
+              image="images/product-01.jpg"
+            ></ProductCard>
+
+            <div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item women">
+              <div class="block2">
+                <div class="block2-pic hov-img0">
+                  <img src="images/product-01.jpg" alt="IMG-PRODUCT" />
+
+                  <a
+                    href="#"
+                    class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1"
+                  >
+                    Quick View
+                  </a>
+                </div>
+
+                <div class="block2-txt flex-w flex-t p-t-14">
+                  <div class="block2-txt-child1 flex-col-l ">
+                    <a
+                      href="product-detail.html"
+                      class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6"
+                    >
+                      Esprit Ruffle Shirt
+                    </a>
+
+                    <span class="stext-105 cl3">$16.64</span>
+                  </div>
+
+                  <div class="block2-txt-child2 flex-r p-t-3">
+                    <a
+                      href="#"
+                      class="btn-addwish-b2 dis-block pos-relative js-addwish-b2"
+                    >
+                      <img
+                        class="icon-heart1 dis-block trans-04"
+                        src="images/icons/icon-heart-01.png"
+                        alt="ICON"
+                      />
+                      <img
+                        class="icon-heart2 dis-block trans-04 ab-t-l"
+                        src="images/icons/icon-heart-02.png"
+                        alt="ICON"
+                      />
+>>>>>>> Stashed changes
                     </a>
                   </div>
                 </div>
